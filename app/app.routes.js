fb23378dff29064275ab7699a30eb4d3738cdeb3
I. Isaku <16718722+IIsaku@users.noreply.github.com>
2018-03-29 20:45:35 +0200

    Fix more linking problems

--- app/app.routes.js
+++ app/app.routes.js
@@ -2,19 +2,19 @@ app.config(function ($routeProvider, $locationProvider) {
     // $locationProvider.html5Mode(true);
     $routeProvider
     .when('/spelers', {
-        templateUrl: '/app/spelers/spelers-view.html',
+        templateUrl: 'app/spelers/spelers-view.html',
         controller: 'spelersController'
     })
     .when('/toernooien', {
-        templateUrl: '/app/toernooien/toernooien-view.html',
+        templateUrl: 'app/toernooien/toernooien-view.html',
         controller: 'toernooienController'
     })
     .when('/speel/:toernooiId', {
-        templateUrl: '/app/speel/speel-view.html',
+        templateUrl: 'app/speel/speel-view.html',
         controller: 'speelController'
     })
     .when('/', {
-        templateUrl: '/app/home/home.html'
+        templateUrl: 'app/home/home.html'
     })
     .otherwise({
         redirectTo: '/'
